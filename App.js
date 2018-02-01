6732ccc37c46abdb0e9ad587e489d2ce48e7aa6a
Robert Ridal <ridal.robert@gmail.com>
2018-02-01 22:39:34 +0200

    moi teksti

--- App.js
+++ App.js
@@ -5,9 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Hello world!</Text>
       </View>
     );
   }
@@ -16,7 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
